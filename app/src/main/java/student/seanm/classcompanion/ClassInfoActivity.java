af74b209a67f3fdab89ca93cbce05c585a20f696
SeanHone <sean.m.hone@gmail.com>
2017-05-21 01:35:08 +1200

    Added navigation drawer to class info page with course names as items

--- app/src/main/java/student/seanm/classcompanion/ClassInfoActivity.java
+++ app/src/main/java/student/seanm/classcompanion/ClassInfoActivity.java
@@ -3,6 +3,8 @@ package student.seanm.classcompanion;
 import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -25,6 +27,13 @@ public class ClassInfoActivity extends AppCompatActivity {
         Intent intent = getIntent();
         courseName = intent.getStringExtra("Class");
         getSupportActionBar().setTitle(courseName);
+
+        //initialise the drawer layout and set the toggle listener
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.classInfo_drawerLayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, tBar,
+                R.string.Nav_Drawer_Open, R.string.Nav_Drawer_Close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
 
         //adapter that deals with having multiple fragments or tabs
         sectionPageAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
